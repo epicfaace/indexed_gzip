58e759d7f782ae73fc30896407262fcf596f5586
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-03-20 16:31:09 -0400

    try using old callmethod function

--- indexed_gzip/zran_file_util.c
+++ indexed_gzip/zran_file_util.c
@@ -116,16 +116,16 @@ int _fseek_python(PyObject *f, int64_t offset, int whence) {
     // because it causes tests with 32-bit OS wheels to fail,
     // so we have to manually build up the arguments instead.
     // TODO: File an issue with cpython about this.
-    // if (data = PyObject_CallMethod(f, "seek", "(l,i)", offset, whence)) == NULL)
+    if (data = PyObject_CallMethod(f, "seek", "(l,i)", offset, whence)) == NULL)
+        goto fail;
+    // if ((seek_fn_name = PyUnicode_FromString("seek")) == NULL)
     //     goto fail;
-    if ((seek_fn_name = PyUnicode_FromString("seek")) == NULL)
-        goto fail;
-    if ((whence_ = PyLong_FromLong(whence)) == NULL)
-        goto fail;
-    if ((offset_ = PyLong_FromLong(offset)) == NULL)
-        goto fail;
-    if ((data = PyObject_CallMethodObjArgs(f, seek_fn_name, offset_, whence_, NULL)) == NULL)
-        goto fail;
+    // if ((whence_ = PyLong_FromLong(whence)) == NULL)
+    //     goto fail;
+    // if ((offset_ = PyLong_FromLong(offset)) == NULL)
+    //     goto fail;
+    // if ((data = PyObject_CallMethodObjArgs(f, seek_fn_name, offset_, whence_, NULL)) == NULL)
+    //     goto fail;
 
     Py_DECREF(data);
     Py_DECREF(seek_fn_name);
